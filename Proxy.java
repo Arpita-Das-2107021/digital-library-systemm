9fe67eb7e0a6310a3b795e2c80c2179504398a09
Arpita Das <141511008+Arpita-Das-2107021@users.noreply.github.com>
2025-09-21 16:25:10 +0600

    proxy

---
+++ Proxy.java
@@ -0,0 +1,46 @@
+public class Proxy{
+    public static void main(String[] args){
+        Ebook e1 = new ProxyEbook("space.pdf");
+        Ebook e2 = new ProxyEbook("space2.pdf");
+        Ebook e3 = new ProxyEbook("space.pdf");
+        Ebook e4 = new ProxyEbook("space4.pdf");
+        Ebook e5 = new ProxyEbook("space.pdf");
+
+        e1.display();        
+        e2.display();
+        e3.display();
+        e4.display();
+        e5.display();
+
+    }
+}
+interface Ebook{
+    void display();
+}
+class RealEbook implements Ebook{
+    private String filename;
+
+    public RealEbook(String filename) {
+        this.filename=filename;
+        loadFromDisk();
+    }
+    private void loadFromDisk(){
+        System.out.println("Ebook "+filename+" loaded from disk");
+    }
+    public void display(){
+        System.out.println("File is "+filename);
+    }
+}
+class ProxyEbook implements Ebook{
+    private RealEbook realEbook;
+    private final String filename;
+    public ProxyEbook(String filename){
+        this.filename=filename;
+    }
+    public void display(){
+        if (realEbook==null){
+            realEbook = new RealEbook(filename);
+        }
+        realEbook.display();
+    }
+}
